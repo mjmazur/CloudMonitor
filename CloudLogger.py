14220980201d2f040e859c345767d8eb3951c94c
mjmazur <mmazur5@uwo.ca>
2022-04-14 22:04:06 +0000

    Update CloudLogger.py

--- CloudLogger.py
+++ CloudLogger.py
@@ -4,12 +4,11 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
-import time
+import time, datetime
 import os
 from socket import *
 from scipy import interpolate
 from logging.handlers import TimedRotatingFileHandler
-from datetime import datetime as dt
 from ftpmod import *
 from time import sleep
 
@@ -22,7 +21,7 @@ def uploadFileFTP(sourceFile1, sourceFile2, server, username, password):
     ftp.storbinary('STOR ' + sourceFile2, open(sourceFile2, 'rb'), 1024)
     ftp.quit()
 
-def fancyPlot(data, imagedir):
+def uptimePlot(data, imagedir):
     print('Making total plot...')
     # Arrange the data into blocks and calculate the mean of each block
     t = data[:,0].astype(np.int)
@@ -61,6 +60,7 @@ def fancyPlot(data, imagedir):
     ax1.set_ylabel('Sky Temp minus Ground Temp (*C)', size=15)
 
     plt.savefig(imagedir + 'CloudCover-Up.png', dpi=200)
+    print('Save all-time cloud cover to %s' % imagedir)
     plt.close()
 
 def plotLog(logname, imagedir):
@@ -73,10 +73,13 @@ def plotLog(logname, imagedir):
 
     # Convert timestamp to hours and put in a column in data
     data['timeh'] = 99
-
+    
     for i in range(len(data.index)):
-        t2 = int(dt.fromtimestamp(data['timestamp'][i]).strftime('%H')) + float(dt.fromtimestamp(data['timestamp'][i]).strftime('%M'))/60 + float(dt.fromtimestamp(data['timestamp'][i]).strftime('%S'))/3600
+        t2 = int(datetime.datetime.fromtimestamp(data['timestamp'][i]).strftime('%H')) + float(datetime.datetime.fromtimestamp(data['timestamp'][i]).strftime('%M'))/60 + float(datetime.datetime.fromtimestamp(data['timestamp'][i]).strftime('%S'))/3600
         data.loc[i,'timeh'] = t2
+
+    # print(datetime.datetime.fromtimestamp(data['timestamp'][10]))
+    # print(int(datetime.datetime.fromtimestamp(data['timestamp'][10]).strftime('%H')) + float(datetime.datetime.fromtimestamp(data['timestamp'][10]).strftime('%M'))/60 + float(datetime.datetime.fromtimestamp(data['timestamp'][10]).strftime('%S'))/3600)
 
     # Arrange the data into blocks and calculate the mean of each block
     # blocksize = 600
@@ -113,6 +116,7 @@ def plotLog(logname, imagedir):
     ax1.set_ylabel('Sky Temp minus Ground Temp (*C)', size=15)
 
     plt.savefig(imagedir + 'CloudCover-Today.png', dpi=200)
+    print('Saved daily image to %s' % imagedir)
     plt.close()
 
 # def sendEmail():
@@ -142,6 +146,12 @@ def main():
     # old_log_size = os.path.getsize(log_file)
 
     cnt = 0
+    try:
+        log_size = os.path.getsize(log_file)
+        old_log_size = log_size
+    except:
+        log_size = 0
+        old_log_size = log_size
 
     while(1):
         req_data = b'All' # Request all data
@@ -151,23 +161,28 @@ def main():
             rec_data, addr = client_socket.recvfrom(2048) # Read the response from arduino
             write_buffer = str(int(time.time())) + ' ' + str(rec_data, 'utf-8') # Format string with Unix time and rec_data
             value_array = np.append(value_array, [write_buffer.split()], axis=0) # Append data to an array for plotting
-            print('Time: ' + str(value_array[cnt,0]) + '   Sky T: ' + str(value_array[cnt,1]) + '   Gnd T: ' + str(value_array[cnt,2]) + '  Delta T: ' + str(float(value_array[cnt,1]) - float(value_array[cnt,2])))
+            print('Time: ' + str(value_array[cnt,0]) + '   Sky T: ' + str(value_array[cnt,1]) + '   Gnd T: ' + str(value_array[cnt,2]) + '  Delta T: ' + str('%.2f' % (float(value_array[cnt,1]) - float(value_array[cnt,2]))))
             # writer.writerow(write_buffer.split()) # Append data to csv file
 
             logger.info(str(value_array[cnt,0]) + ',' + str(value_array[cnt,1]) + ',' + str(value_array[cnt,2]))
-            print(value_array[cnt,1])
-            # log_size = os.path.getsize(log_file)
-            # if log_size < old_log_size:
-            #     os.rename(image_dir + "CloudCover-Today.png", image_dir + "CloudCover-" + (dt.datetime.now()-dt.timedelta(1)).strftime("%Y%m%d"))
-  
-            # old_log_size = log_size
+            # print(value_array[cnt,1])
+
+            log_size = os.path.getsize(log_file)
+            print('Log file size is: %s' % str(log_size))
+            print('Old log file was: %s' % str(old_log_size))
+            if log_size < old_log_size:
+                os.rename(image_dir + "CloudCover-Today.png", image_dir + "CloudCover-" + (datetime.datetime.now()-datetime.timedelta(1)).strftime("%Y%m%d") + ".png")
+                print('Moved cloud cover image.')
+
+            print('Plotting...')
+            old_log_size = log_size
 
             cnt += 1
 
-            if cnt % 10 == 0:
-                fancyPlot(value_array, image_dir)
+            if cnt % 4 == 0:
+                uptimePlot(value_array, image_dir)
 
-            if cnt % 30 == 0:
+            if cnt % 8 == 0:
                 plotLog(log_file, image_dir)
                 # uploadFileFTP(image_dir + 'CloudCover-Today.png', image_dir + 'CloudCover-Up.png', server, username, password)
 
